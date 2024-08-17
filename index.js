513ecd4aa4fd6bb10773734378e9b043bf298e56
web3-diplomat <web3diplomat@yahoo.com>
2024-08-17 16:24:08 -0400

    creation of public and private key

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const {
 } = require("@solana/web3.js")
 
 const wallet = new Keypair()
+
+const publicKey = wallet._keypair.publicKey
+const secretKey = wallet._keypair.secretKey
+
+console.log(publicKey)
+console.log(secretKey)
