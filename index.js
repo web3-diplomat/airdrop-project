74bc577adf1025dabd9bd7507ef8e9886b02bf7a
web3-diplomat <web3diplomat@yahoo.com>
2024-08-17 16:47:26 -0400

    added functionality to getwalletbalance function/ connection obj created web3.js modules and 'devnet'

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const secretKey = wallet._keypair.secretKey
 
 const getWalletBalance = async() => {
     try{
-        
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
     } catch(err) {
         console.error(err)
     }
