9da58e361834fb945c19224b390f757e701d2f38
web3-diplomat <web3diplomat@yahoo.com>
2024-08-17 17:34:09 -0400

    create main function to call getWalletBalance and then execute main funciton

--- index.js
+++ index.js
@@ -15,9 +15,12 @@ const getWalletBalance = async() => {
     try{
         const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
         const walletBalance = await connection.getBalance(publicKey)
-        console.log('Wallet balance is' ${walletBalance})
+        console.log(`Wallet balance is ${walletBalance}`)
     } catch(err) {
         console.error(err)
     }
 }
-
+const main = async() => {
+    await getWalletBalance()
+}
+main()
